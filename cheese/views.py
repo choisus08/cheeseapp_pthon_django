ddcf87a642781bda592a5a091972dd3be3100f35
Susie Gordon <susieyoungchoi@gmail.com>
2023-09-18 11:21:26 -0400

    created viewset

--- cheese/views.py
+++ cheese/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Cheese
+from rest_framework import viewsets, permissions
+from .serializers import CheeseSerializer
 
 # Create your views here.
+
+class CheeseViewSet(viewsets.ModelViewSet):
+    # main query for the index route
+    queryset = Cheese.objects.all()
+    # serializer class for serializing output
+    serializer_class = CheeseSerializer
+    # optional permission class set permission level
+    permission_classes = [permissions.AllowAny]
